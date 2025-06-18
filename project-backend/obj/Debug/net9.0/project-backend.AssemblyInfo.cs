edbb22aa9ec5ccc22c713b9abed9751c797013be
Loweso <dlouisefermin@gmail.com>
2025-06-18 16:53:01 +0800

    README edit

--- project-backend/obj/Debug/net9.0/project-backend.AssemblyInfo.cs
+++ project-backend/obj/Debug/net9.0/project-backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50646def1c76f58ebdf6af49529b1a9d7931d4b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67c6d5a1dc62560085a1547da7f59458f5c1de0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("project-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
